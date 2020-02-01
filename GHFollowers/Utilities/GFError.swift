899f22a184c91e6127499022ef27ac4fa60ef432
Chris Conner <chris.conner@gmail.com>
2020-01-31 23:43:16 -0600

    Added persistence

--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -9,8 +9,10 @@
 import Foundation
 
 enum GFError: String, Error {
-    case invalidUsername = "This username created an invalide request. Please try again."
-    case unableToComplete = "Unable to complete your request. Please check your internet connection"
-    case invalidResponse = "Invalid response from the server. Please try again"
-    case invalidData = "The data received from the server was invalid. Please try agai"
+    case invalidUsername    = "This username created an invalide request. Please try again."
+    case unableToComplete   = "Unable to complete your request. Please check your internet connection"
+    case invalidResponse    = "Invalid response from the server. Please try again"
+    case invalidData        = "The data received from the server was invalid. Please try agai"
+    case unableToFavorite   = "There was an error favoriting this user. Please try again."
+    case alreadyInFavorites = "You have already added this user."
 }
